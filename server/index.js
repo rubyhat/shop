da96c10ff1975d5c2e07be9ef60419140ea49cdd
Vladislav <dreamervt8@gmail.com>
2022-06-17 13:40:19 +0600

    add basic models

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
+const models = require("./models");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "working" });
+});
 
 const start = async () => {
   try {
