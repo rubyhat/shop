e058ce134657c8bd1ddb725b77cfe471d2ae8d15
Vladislav <dreamervt8@gmail.com>
2022-07-08 19:23:22 +0600

    add city modal

--- client/src/components/SubHeader/index.tsx
+++ client/src/components/SubHeader/index.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import { Col, Container, Row } from "../Helpers/Layout";
 import classNames from "classnames/bind";
 
 import styles from "./styles.module.scss";
+import Modal from "../Helpers/Modal";
+import Button from "../Helpers/Button";
 
 const SubHeader = () => {
   const cn = classNames.bind(styles);
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className={cn("sub-header")}>
+      {showModal && (
+        <Modal className={cn("modal-city")}>
+          <h3>Выбор города</h3>
+          <p>
+            В данный момент мы работаем только в городе{" "}
+            <strong>Караганда</strong>, но уже очень скоро мы откроемся на весь{" "}
+            <strong>Казахстан!</strong>
+          </p>
+          <Button
+            onClick={() => setShowModal(false)}
+            variant="primary"
+            text="Продолжить"
+          />
+        </Modal>
+      )}
       <Container>
         <Row>
           <Col>
             <div className="d-flex align-items-center justify-content-between">
-              <p>Караганда</p>
+              <button
+                onClick={() => setShowModal(true)}
+                className={cn("button")}
+              >
+                Караганда
+              </button>
               <p>+7 (701) 123 45 67 | с 9:00 до 18:00</p>
             </div>
           </Col>
